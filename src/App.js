f40213b0e5f16e0dabeb87dc23c84adbf5cea455
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-02-28 23:57:32 +0000

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 export default function App() {
+
+    // https://api.openweathermap.org/data/2.5/weather?q=dublin&APPID=86bcfce152dd714021901d8484a429ac
     return (
         <>
-            <h1>Hola Emilio</h1>
+            <div className="container">
+                <div className="top">
+                    <div className="city">
+                        Dublin
+                    </div>
+                    <div className="temperature">
+                        10 C
+                    </div>
+                    <div className="description">
+                        Raining
+                    </div>
+                </div>
+                <div className="bottom">
+
+                </div>
+            </div>
         </>
     )
 }
