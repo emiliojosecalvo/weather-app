8aadc39fdb6acde3f59346da6c4c944321e8b378
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-03-01 00:16:35 +0000

    adding steps to build the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import React, { useState } from "react";
+
 export default function App() {
+    //States to consult the location and the data that is gonna come back from the api
 
     // https://api.openweathermap.org/data/2.5/weather?q=dublin&APPID=86bcfce152dd714021901d8484a429ac
+
+    //consult API with axios
+
+    //input to be able to add the city that we gonna consult
+
+    //show the main data on the app
+
+    //Style the app
     return (
         <>
             <div className="container">
